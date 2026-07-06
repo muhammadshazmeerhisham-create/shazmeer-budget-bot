db87721afc492ea07a4ab23773d50a4a3b1ed781
muhammadshazmeerhisham-create <muhammadshazmeerhisham@gmail.com>
2026-07-06 11:57:14 +0800

    PHOTO FUNCTION CALLED

--- bot.py
+++ bot.py
@@ -64,6 +64,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # ==========================
 
 async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("PHOTO FUNCTION CALLED")
 
     print("===== PHOTO RECEIVED =====")
 
