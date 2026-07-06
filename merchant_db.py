85ae0d5c28fea6ffa1c7805b5f6fb8253c815d5f
muhammadshazmeerhisham-create <muhammadshazmeerhisham@gmail.com>
2026-07-06 16:02:09 +0800

    Add merchant database

---
+++ merchant_db.py
@@ -0,0 +1,83 @@
+MERCHANT_DB = {
+
+    "MASLEE": {
+        "name": "Maslee",
+        "category": "Grocery"
+    },
+
+    "99 SPEEDMART": {
+        "name": "99 Speedmart",
+        "category": "Grocery"
+    },
+
+    "LOTUSS": {
+        "name": "Lotus's",
+        "category": "Grocery"
+    },
+
+    "LOTUS'S": {
+        "name": "Lotus's",
+        "category": "Grocery"
+    },
+
+    "MYDIN": {
+        "name": "Mydin",
+        "category": "Grocery"
+    },
+
+    "ECONSAVE": {
+        "name": "Econsave",
+        "category": "Grocery"
+    },
+
+    "AEON": {
+        "name": "AEON",
+        "category": "Shopping"
+    },
+
+    "AEON BIG": {
+        "name": "AEON Big",
+        "category": "Grocery"
+    },
+
+    "TF MART": {
+        "name": "TF Mart",
+        "category": "Grocery"
+    },
+
+    "TF VALUE-MART": {
+        "name": "TF Value-Mart",
+        "category": "Grocery"
+    },
+
+    "TF VALUEMART": {
+        "name": "TF Value-Mart",
+        "category": "Grocery"
+    },
+
+    "KK MART": {
+        "name": "KK Mart",
+        "category": "Convenience Store"
+    },
+
+    "FAMILYMART": {
+        "name": "FamilyMart",
+        "category": "Convenience Store"
+    },
+
+    "SHELL": {
+        "name": "Shell",
+        "category": "Petrol"
+    },
+
+    "PETRONAS": {
+        "name": "Petronas",
+        "category": "Petrol"
+    },
+
+    "GXBANK": {
+        "name": "GXBank",
+        "category": "Transfer"
+    }
+
+}
