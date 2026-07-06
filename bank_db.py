b5b7c8cdbe52116c9d7706edd73e7c62b71c0cda
muhammadshazmeerhisham-create <muhammadshazmeerhisham@gmail.com>
2026-07-06 19:24:48 +0800

    Create bank_db.py

---
+++ bank_db.py
@@ -0,0 +1,48 @@
+BANK_DB = {
+
+    "GXBANK": {
+        "name": "GXBank",
+        "category": "Transfer"
+    },
+
+    "GXBANK PAYMENT SUCCESSFUL": {
+        "name": "GXBank",
+        "category": "Transfer"
+    },
+
+    "DUITNOW": {
+        "name": "DuitNow",
+        "category": "Transfer"
+    },
+
+    "TOUCH 'N GO": {
+        "name": "TNG eWallet",
+        "category": "E-Wallet"
+    },
+
+    "TNG EWALLET": {
+        "name": "TNG eWallet",
+        "category": "E-Wallet"
+    },
+
+    "MAE": {
+        "name": "Maybank MAE",
+        "category": "Transfer"
+    },
+
+    "MAYBANK2U": {
+        "name": "Maybank2u",
+        "category": "Transfer"
+    },
+
+    "CIMB": {
+        "name": "CIMB Clicks",
+        "category": "Transfer"
+    },
+
+    "BANK ISLAM": {
+        "name": "Bank Islam",
+        "category": "Transfer"
+    }
+
+}
