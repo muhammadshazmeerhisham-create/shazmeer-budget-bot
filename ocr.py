2b4f569d4194e779b751706cc42afa1bc9d68223
muhammadshazmeerhisham-create <muhammadshazmeerhisham@gmail.com>
2026-07-06 14:58:58 +0800

    Add receipt scanning function with OCR API integration
    Implement receipt scanning functionality using OCR API.

--- ocr.py
+++ ocr.py
@@ -2,6 +2,34 @@ import requests
 
 from config import OCR_API_KEY
 
+
 def scan_receipt(filename):
     print("OCR Module Loaded")
-    return ""
+
+    with open(filename, "rb") as f:
+        response = requests.post(
+            "https://api.ocr.space/parse/image",
+            files={"filename": f},
+            data={
+                "apikey": OCR_API_KEY,
+                "language": "eng"
+            }
+        )
+
+    result = response.json()
+
+    print("========== OCR DEBUG ==========")
+    print("Status Code:", response.status_code)
+    print(result)
+    print("===============================")
+
+    text = ""
+
+    if result.get("ParsedResults"):
+        text = result["ParsedResults"][0]["ParsedText"]
+
+    print("===== OCR TEXT =====")
+    print(repr(text))
+    print("====================")
+
+    return text
